d3a6d8afb061f44eafbda2126950b57c6a0a7a2e
Avinash Wadhawan <avinashxw@gmail.com>
2021-07-12 23:37:38 +0530

    craete-category-form-view

--- resources/views/admin/category/add.blade.php
+++ resources/views/admin/category/add.blade.php
@@ -2,6 +2,25 @@
 
 @section('container')
 <div class="row">
-    <h3>Create Category</h3>
+    <h3 class="mb-10">Create Category</h3>
+</div>
+<div class="row">
+    <a href="category"><button class="btn btn-success">Back</button></a>
+</div>
+<div class="row">
+<form action="" method="post" style="width: 100%; padding-top: 20px;">
+    @csrf
+    <div class="mb-3">
+        <label for="categoryname" class="form-label">Name</label>
+        <input type="email" class="form-control" placeholder="XYZ..">
+    </div>
+    <div class="mb-3">
+        <label for="categorydescription" class="form-label">Description</label>
+        <textarea class="form-control" rows="3"></textarea>
+    </div>
+    <div class="mb-3">
+        <button type="submit" class="btn btn-primary">Create</button>
+    </div>
+</form>
 </div>
 @endsection
